d6b1c61d4c75c94ea2f62d6cf89ebdbc486d6369
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-13 13:05:41 -0700

    Getting started with c-programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 * main - Main function of the program
 * Return: Always 0 (Success)
 */
-int main(void) {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
