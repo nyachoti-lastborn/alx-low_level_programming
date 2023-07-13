ec325b307386187eddc54c9e32bb3cf90ed63c79
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-13 13:34:39 -0700

    Alternative of printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - Function that prints main objective of the program
+* return (1): after succesful running of the program should return a value of 1
+*/
+int main (void)
+{write(1,""and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (1)
+}
