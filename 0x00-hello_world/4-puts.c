c9797360485bb6b15d77193e4706363b6dacd464
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-13 13:08:46 -0700

    Getting started with C-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
 * main - print the string in the put function 
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
