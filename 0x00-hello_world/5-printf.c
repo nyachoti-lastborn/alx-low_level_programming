b8f570245085b9459b23ed987d7a1be7d06d9d7d
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-13 12:50:13 -0700

    Getting started with C-Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *This program prints out the output of function printf
- */
-int main (){
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;}
+*This program prints out the output of function printf
+*/
+int main (void) {
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- *this program prints the size of the datatypes.
- */
-int main() {
-    printf("Size of char: %d byte(s)\n", sizeof(char));
-    printf("Size of short: %d byte(s)\n", sizeof(short));
-    printf("Size of int: %d byte(s)\n", sizeof(int));
-    printf("Size of long int: %d byte(s)\n", sizeof(long));
-    printf("Size of long long int: %d byte(s)\n", sizeof(long long));
-    printf("Size of float: %d byte(s)\n", sizeof(float));
-    
-  return 0;
+*this program prints the size of the datatypes.
+*/
+int main(void) {
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a short: %d byte(s)\n", sizeof(short));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }
