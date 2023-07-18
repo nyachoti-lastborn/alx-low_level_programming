8e760a263f698bfcccb07d7c9d7cc7f32b2e6590
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-18 15:54:02 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  *_islower- function to check if value is lower
+ * c - parameter to be checked
+ * return (1)-returns value of 1 if true
  *return (0)- returns a value of 0
 */
 int _islower(int c)
