fe38dca7d77b151ec9102fdd7f599ea7c7e72208
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-18 14:07:25 -0700

    Nested Loops and functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,6 @@
 */
 int main (void)
 {
-  printf("_putchar\n");
-  return (0)
+_putchar("_putchar\n");
+return (0)
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <main.h>
 /**
  *Description- code prints the alphabet
  * in lowercase
- * void print_alphabet - function prototype for printing
+ * void print_lowercase- function prototype for printing
  * the alphabet
  * return (0) -returns 0
 */
-void print_alphabet(void)
+void print_lowercase(void)
 {
  char ch;
 for (ch = 'a', ch <='z', ch++)
