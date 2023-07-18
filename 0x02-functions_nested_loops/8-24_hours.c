a756a19473c1294e355586be18f6bbb201a22349
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-18 16:25:48 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,21 @@
  */
 void jack_bauer(void)
 {
-for (int hour = 0; hour < 24; hour++)
+int hour, minute;
+hour = 0;
+while (hour <= 23)
 {
-for (int minute = 0; minute < 60; minute++)
+minute = 0;
+while (minute <= 59)
 {
-int hourTens = hour / 10;
-int hourOnes = hour % 10;
-int minuteTens = minute / 10;
-int minuteOnes = minute % 10;
-_putchar(hourTens + '0');
-_putchar(hourOnes + '0');
+_putchar('0' + hour / 10);
+_putchar('0' + hour % 10);
 _putchar(':');
-_putchar(minuteTens + '0');
-_putchar(minuteOnes + '0');
+_putchar('0' + minute / 10);
+_putchar('0' + minute % 10);
 _putchar('\n');
+++minute;
 }
+++hour;
 }
 }
