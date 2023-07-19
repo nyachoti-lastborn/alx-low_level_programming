c5e0556f4c58c431bad640117ac1cc2ad0157d6c
nyachoti-lastborn <jmogaka84@gmail.com>
2023-07-19 09:52:34 -0700

    Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+#end if
